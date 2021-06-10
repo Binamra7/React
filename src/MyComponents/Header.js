68369a24657b2b43680f60cf6b099b157e7b2a79
Binamra7 <binamra7khadka6@gmail.com>
2021-06-10 21:32:29 +0545

    M

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -14,7 +14,7 @@ export default function Header(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <a className="nav-link active" aria-current="page" href="http://www.github.com/Binamra7">GitHub</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">About</a>
